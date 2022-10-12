6e3b44a2206aa843cd7aef5bc9b098fac6f2290d
shwetamutalikdesai <102358266+shwetamutalikdesai@users.noreply.github.com>
2022-10-12 12:24:15 +0530

    Create interface.sv

---
+++ SWITCH/interface.sv
@@ -0,0 +1,9 @@
+interface intf(input clk, rstn);
+  logic [7:0]addr;
+  logic [15:0]data;
+logic vld;
+  logic [7:0]addr_a;
+  logic [15:0]data_a;
+  logic [7:0]addr_b;
+  logic [15:0]data_b;
+endinterface
